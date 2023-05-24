15f52e5fbb16489512a3e3023979de47324bf4d4
sreejitha <sreejitha@webandcrafts.in>
2023-05-24 16:55:15 +0530

    todo list completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Counter from './Counter';
+import {
+  BrowserRouter as Router, Switch, Route,
+  BrowserRoutes,
+  Routes
+} from 'react-router-dom';
+import Todolist from './TodoList';
 
 function App() {
   return (
-    <div>
-      <Counter />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Counter />} />
+        <Route path="/todo" element={<Todolist />} />
+      </Routes>
+
+    </Router>
   );
-  
+
 }
+
 export default App;
