14f49474c9843183747acd68196e4fc668a25510
sreejitha <sreejitha@webandcrafts.in>
2023-05-24 10:06:03 +0530

    app.js updated

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import React, { useState } from 'react';
 function Counter() {
