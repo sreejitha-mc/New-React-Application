54309ffb4b1f0c1883cdb8645779a83d9b3ba599
sreejitha <sreejitha@webandcrafts.in>
2023-05-30 21:47:27 +0530

    memo added

--- src/pages/DisplayArrayData/DisplayArrayData.js
+++ src/pages/DisplayArrayData/DisplayArrayData.js
@@ -1,39 +1,53 @@
-import React from 'react';
+import React, { memo } from 'react';
 import './DisplayArrayData.css';
-import useDisplayArray from '../../logic/useDiplayArray'
+import useDisplayArray from '../../logic/useDiplayArray';
+
+const InputComponent = memo(({ id, text, onChange }) => {
+    console.log('in')
+  return (
+    <input
+      value={text}
+      onChange={(e) => onChange(id, e.target.value)}
+      className="input-box"
+    />
+  );
+});
 
 const DisplayArrayComponent = () => {
-    const initialData = [
-        { id: 1, text: 'First item' },
-        { id: 2, text: 'Second item' },
-        { id: 3, text: 'Third item' },
-      ];
-    
-      const [data, handleChange] = useDisplayArray(initialData);
-    
-      return (
-        <div className="container">
-          <div className="editable-items">
-            {data.map((item) => (
-              <input
-                key={item.id}
-                value={item.text}
-                onChange={(e) => handleChange(item.id, e.target.value)}
-                className="input-box"
-              />
-            ))}
+  const initialData = [
+    { id: 1, text: '' },
+    { id: 2, text: '' },
+    { id: 3, text: '' },
+  ];
+
+  const [data, handleChange] = useDisplayArray(initialData);
+
+  return (
+    <div className="container">
+      <div className="editable-items">
+        {data.map((item) => (
+            <div>
+          <InputComponent
+            key={item.id}
+            id={item.id}
+            text={item.text}
+            onChange={handleChange}
+          />
           </div>
-          <div className="current-data">
-            <h2>Current Array Data:</h2>
-            <ul>
-              {data.map((item) => (
-                <li key={item.id}>{item.text}</li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      );
-    
+        ))}
+      </div>
+      <div className="current-data">
+        <h2>Current Array Data:</h2>
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>
+              {item.id} {item.text}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default DisplayArrayComponent;
